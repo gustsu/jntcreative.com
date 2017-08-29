5343a731ced50b1f62f37f15ab9d2c422902ac7b
Justin Tew <justin-@live.com>
2017-08-28 21:39:05 -0700

    finishing up services section

--- assets/src/js/scripts.js
+++ assets/src/js/scripts.js
@@ -1,9 +1,24 @@
-var prev = 0;
-var $window = $(window);
-var nav = $('nav');
-
-$window.on('scroll resize', function(){
-  var scrollTop = $window.scrollTop();
-  nav.toggleClass('nav-down', scrollTop > prev);
-  prev = scrollTop;
+$(document).scroll(function() { 
+   if($(window).scrollTop() === 0) {
+     $("#main-nav").removeClass("nav-down").addClass('nav-up');
+   }
+   else {
+     $("#main-nav").addClass("nav-down").addClass('nav-up');
+   }
 });
+
+$(function(){
+	 $('.mobile-menu-button').on('click', function(){
+	 	$('.right-side .nav-links').addClass('transition');
+	 	$('#main-nav').addClass('mobile-menu');
+	 	$('.mobile-menu-button').hide();
+	 	$('.mobile-menu-exit-button').show();
+	 });
+	$('.mobile-menu-exit-button').on('click', function(){
+		$('.right-side .nav-links').removeClass('transition');
+		$('.mobile-menu-button').show();
+	 	$('.mobile-menu-exit-button').hide();
+	 	$('#main-nav').removeClass('mobile-menu');
+
+	 });
+})
